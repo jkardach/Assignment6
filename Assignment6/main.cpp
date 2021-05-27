ba9fda388c5f40ad025d59030f14a779daa3d196
Jim Kardach <jim@kardach.clom>
2021-05-27 16:57:46 -0700

    Initial Commit

---
+++ Assignment6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment6
+//
+//  Created by jim kardach on 5/27/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
